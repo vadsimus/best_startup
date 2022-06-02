b360f2118d6db4c3d9dff796a54b32f14213c96c
vadsimus <vadsimus@gmail.com>
2022-06-02 09:00:03 +0300

    add github api test

--- best-startup/src/Users.js
+++ best-startup/src/Users.js
@@ -1,5 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Users = () => (
-    <h1>Page FOR Users</h1>
-)
+function Users() {
+    const [ search, setSearch ] = useState(0)
+    const [ output, setOutput ] = useState('[]')
+
+
+function SearchButtonHandler() {
+    fetch('https://api.github.com/users/' + search + '/repos')
+    .then(response => response.json())
+    .then(repos => {
+        let q = []
+        repos.forEach((elem) => {
+            q.push(elem.name)
+            })
+        setOutput(JSON.stringify(q))
+        
+        }
+        )
+    }
+
+    return (
+        <>
+        <h1>Page FOR Users</h1>
+        <nav class="navbar navbar-light bg-light">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e)=>{setSearch(e.target.value)}}/>
+        <button class="btn btn-outline-success my-2 my-sm-0" onClick={SearchButtonHandler}>Search</button>
+    </nav>
+
+    <div>
+        {JSON.parse(output).map( (item, index) => (
+            <div key={index}>{item}</div>
+        )
+        )
+}
+           
+    </div>
+    </>
+    )
+}
+
+
+export {Users}
